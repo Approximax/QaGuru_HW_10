8c02137bd377059a4e5f9e3f615f188c146decb3
Vladislav Kryukov <vlad_pw_v@list.ru>
2024-02-01 20:08:34 +0300

    Test data and csv parametrized test

--- src/test/java/parametrized/SimpleParametrizedTests.java
+++ src/test/java/parametrized/SimpleParametrizedTests.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -26,8 +29,18 @@ public class SimpleParametrizedTests extends TestBase{
     void searchResultsShouldNotBeEmpty(String searchQueryInput) {
         $("[placeholder='Искать в Самокате']").setValue(searchQueryInput);
 
-        $("")
+        $$(".ProductCardOverlay_slide__954Wj").shouldBe(sizeGreaterThan(0));
+    }
 
-
+    @Tag("BLOCKER")
+    @CsvSource(value = {
+            "Хлеб и выпечка , Хлеб",
+            "Мясо и рыба , Мясо и выпечка"
+    })
+    @CsvFileSource(resources = "/internalCategoriesMatchWithParent.csv")
+    @ParameterizedTest(name = "Соответствие внутренней категории её родительской")
+    void internalCategoriesMatchWithParent(String internalCategory, String parentCategory) {
+        $(".CatalogTree_root__IHeCU").find(String.valueOf(text(parentCategory))).click();
+        $("CatalogTreeSectionCard_categories__4uYFm").shouldHave(text(internalCategory));
     }
 }
--- src/test/java/parametrized/TestBase.java
+++ src/test/java/parametrized/TestBase.java
@@ -1,10 +1,10 @@
 package parametrized;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeEach
+    void beforeEach() {
         Configuration.baseUrl = "https://samokat.ru/";
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 5000;
